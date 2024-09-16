ce5e831d7c8433c03e1f3d2d0a6936acbbf5262b
MitinNV <Tyuiu.MitinNV@outlook.com>
2024-09-16 19:27:19 +0500

    Task4

---
+++ Tyuiu.MitinNV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.MitinNV.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(1, 5));
+Console.WriteLine(DataService.Multiplication(9, 8));
+Console.WriteLine(DataService.Division(999, 9));
+
+Console.ReadKey();
