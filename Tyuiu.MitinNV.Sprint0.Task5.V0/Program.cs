344f20ad679f09ef3ea845d3b92277119be8b4ad
MitinNV <Tyuiu.MitinNV@outlook.com>
2024-09-16 21:11:40 +0500

    Task5

---
+++ Tyuiu.MitinNV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.MitinNV.Sprint0.Task5.V0.Lib;
+
+Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 50));
+Console.WriteLine("A / B = " + DataService.Division(10, 2));
+Console.ReadKey();
